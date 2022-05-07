ff30848f28b9ab60bfcd8f3de10638d6142fdd9a
QuachPhiHoang <hoangquach1908@gmail.com>
2022-05-07 21:11:07 +0700

    bài 61

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
-    return <h1>React app</h1>;
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Pages = route.components;
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Pages />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -0,0 +1,5 @@
+function Sidebar() {
+    return <header>Sidebar</header>;
+}
+
+export default Sidebar;
