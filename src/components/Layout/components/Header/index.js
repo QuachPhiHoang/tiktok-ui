55b8875adbec5aeea1e73d67885da9ba145e0c8d
QuachPhiHoang <hoangquach1908@gmail.com>
2022-05-08 19:06:26 +0700

    bài 64

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,48 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Header.module.scss';
+
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input
+                        placeholder="search accounts and videos"
+                        spellCheck={false}
+                        type="text"
+                        className={cx('search-input')}
+                    />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon className={cx('search-btn-icon')} icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}>
+                    <div className={cx('upload')}>
+                        <a>Upload</a>
+                    </div>
+                    <div className={cx('log-in')}>
+                        <a>Login</a>
+                    </div>
+                    <div className={cx('setting')}>
+                        <FontAwesomeIcon icon={faEllipsisVertical} />
+                    </div>
+                </div>
+            </div>
         </header>
     );
 }
