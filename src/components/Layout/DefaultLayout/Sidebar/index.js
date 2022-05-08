2fef2e5483aace309c54afb05011b0c5bb9de054
QuachPhiHoang <hoangquach1908@gmail.com>
2022-05-08 15:59:52 +0700

    bài 63

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <header>Sidebar</header>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2 className={cx('sidebar')}>Sidebar</h2>
+        </aside>
+    );
 }
 
 export default Sidebar;
